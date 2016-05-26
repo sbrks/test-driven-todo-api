34ea6212b278543992327b423e24bae1c23b5234
SBRKS <sbrks3@gmail.com>
2016-05-26 08:51:48 -0700

    updates edit, update, and delete functions

--- server.js
+++ server.js
@@ -9,15 +9,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // serve static files from public folder
 app.use(express.static(__dirname + '/public'));
 
+
 /************
  * DATABASE *
  ************/
 
 // our database is an array for now with some hardcoded values
 var todos = [
-  // { _id: 1, task: 'Laundry', description: 'Wash clothes' },
-  // { _id: 2, task: 'Grocery Shopping', description: 'Buy dinner for this week' },
-  // { _id: 3, task: 'Homework', description: 'Make this app super awesome!' }
+  { _id: 1, task: 'Laundry', description: 'Wash clothes' },
+  { _id: 2, task: 'Grocery Shopping', description: 'Buy dinner for this week' },
+  { _id: 3, task: 'Homework', description: 'Make this app super awesome!' }
 ];
 
 /**********
@@ -50,27 +51,79 @@ app.get('/api/todos/search', function search(req, res) {
 });
 
 app.get('/api/todos', function index(req, res) {
+  var idname = req.body._id;
+  var description = req.body.description;
+  var task = req.body.task;
   /* This endpoint responds with all of the todos
    */
-});
+   //what are you going to send back to the client?
+    return res.json({todos: todos});
+    res.render({idname: idname, description: description, task: task});
+    //todo objects should have properties _id, description, task
+
+   });
 
 app.post('/api/todos', function create(req, res) {
   /* This endpoint will add a todo to our "database"
    * and respond with the newly created todo.
    */
+
+    var todo = new Todo({'_id': req.params.id, 'task': req.params.task, 'description': req.params.description});
+    console.log(todo);
+
+    //i 
+
+    todo.save(function(err) {
+      if(err) res.json({message: 'Could not create todo because ' + err});
+      res.redirect("/todos");
+    });
 });
 
 app.get('/api/todos/:id', function show(req, res) {
+  var id = req.params.id;
   /* This endpoint will return a single todo with the
    * id specified in the route parameter (:id)
    */
+
+   if (id == "task") {
+    console.log("yes, new todo!");
+    res.render("task");
+   } else {
+   todos.findById({_id: id}, function (err, todo) {
+    if(err) {
+        res.json({message: 'Could not find todo because error: ' + err});
+      } else
+      console.log(res.params);
+      res.json({todos: todos});
+      res.render('./index.js', {todos: todos});
+   });
+ }
 });
+
 
 app.put('/api/todos/:id', function update(req, res) {
   /* This endpoint will update a single todo with the
    * id specified in the route parameter (:id) and respond
    * with the newly updated todo.
    */
+
+
+var id = req.params.id;
+todos.findById({_id: id}, function (err, todo) {
+  if (err) res.json ({message: 'Could not updated todo: ' + err});
+
+  if (req.params.text) {
+    todo.text = req.params.text;
+  }
+
+  console.log(todo);
+  todo.save(function(err) {
+    if(err) res.json({message: 'Could not save updated todo: ' + err});
+
+    res.redirect('/app');
+  });
+});
+
 });
 
 app.delete('/api/todos/:id', function destroy(req, res) {
@@ -78,13 +131,20 @@ app.delete('/api/todos/:id', function destroy(req, res) {
    * id specified in the route parameter (:id) and respond
    * with success.
    */
+
+   var id = req.params.id;
+   todo.remove({_id: id}, function (err) {
+    if (err) res.json ({message: 'Could not delete: ' + err});
+
+    res.redirect('/app');
+   });
 });
 
 /**********
  * SERVER *
  **********/
 
-// listen on port 3000
+// listen on port 3000, start with node server.js
 app.listen(3000, function() {
   console.log('Server running on http://localhost:3000');
 });
